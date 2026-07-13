9acb15c00dd46c184e31cf9506aaf43f1ce773d9
dishasisodia01 <dishasisodia1@gmail.com>
2026-07-12 22:08:30 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.title("Customer Churn Prediction")
+st.write("This project was developed by Disha Balram Singh Sisodia as part of the unified Mentor Intership.")
+st.success("Customer Churn Prediction Model")
+st.write("This is a demo deployment of the customer churn prediction project.")
